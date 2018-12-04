be82119c6678f1a3663f2d180009dcdf0cc49f7f
Jonas Nikula <jonas.nikula@gmail.com>
2018-12-04 22:27:51 +0200

    Add findRepeating function

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,8 +4,14 @@ main = do
   contents <- readFile "input"
   let strList = lines $ stripChars "+" contents
   let intList = map read strList
-  let result = sum intList
+  let result = findRepeating [0] intList
   print result
 
 stripChars :: String -> String -> String
 stripChars = filter . flip notElem
+
+findRepeating :: [Int] -> [Int] -> Int
+findRepeating sumList intList
+  | elem curr sumList = curr
+  | otherwise = findRepeating (curr:sumList) (tail intList)
+  where curr = (head sumList) + (head intList)
