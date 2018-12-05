a1ffc823a7712abbbf3a3cc970a4a5bfc2f3b2d1
Jonas Nikula <jonas.nikula@gmail.com>
2018-12-06 00:19:36 +0200

    Solve day1.2

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,14 +4,15 @@ main = do
   contents <- readFile "input"
   let strList = lines $ stripChars "+" contents
   let intList = map read strList
-  let result = findRepeating [0] intList
+  let result = findRepeating [0] intList intList
   print result
 
 stripChars :: String -> String -> String
 stripChars = filter . flip notElem
 
-findRepeating :: [Int] -> [Int] -> Int
-findRepeating sumList intList
+findRepeating :: [Int] -> [Int] -> [Int] -> Int
+findRepeating sumList [] origList = findRepeating sumList origList origList
+findRepeating sumList intList origList
   | elem curr sumList = curr
-  | otherwise = findRepeating (curr:sumList) (tail intList)
+  | otherwise = findRepeating (curr:sumList) (tail intList) origList
   where curr = (head sumList) + (head intList)
