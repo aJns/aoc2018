d21f861461c93624d4b7c9d7f3c09e4a2d55e361
Jonas Nikula <jonas.nikula@student.tut.fi>
2018-12-31 19:53:31 +0200

    Solve day2.2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,32 +1,38 @@
 import System.IO
-import Data.Map
-import Data.List
 
 
 main = do
     contents <- readFile "input"
     let ids = lines contents
-    let result = generateChecksum ids 0 0
+    let result = findCommonLetters ids
     print result
 
 
-charCounts :: String -> [Int]
-charCounts word = Prelude.map length $ group $ sort word
+countDifferences :: String -> String -> Int -> Int
+countDifferences [] [] count = count
+countDifferences (a:as) (b:bs) count
+    | a /= b = countDifferences as bs (count + 1)
+    | otherwise = countDifferences as bs count
 
 
-hasTwo :: [Int] -> Bool
-hasTwo counts = elem 2 counts
+commonLetters :: String -> String -> String -> String
+commonLetters (a:as) (b:bs) sameSoFar
+    | a == b = commonLetters as bs (sameSoFar ++ [a])
+    | a /= b = sameSoFar ++ as
 
 
-hasThree :: [Int] -> Bool
-hasThree counts = elem 3 counts
+findDifferByOne :: String -> [String] -> String
+findDifferByOne base [] = []
+findDifferByOne base (x:xs)
+    | differByOne = commonLetters base x []
+    | otherwise = findDifferByOne base xs
+    where differByOne = 1 == countDifferences base x 0
 
 
-generateChecksum :: [String] -> Int -> Int -> Int
-generateChecksum [] twos threes = twos * threes
-generateChecksum (x:xs) twos threes
-    | hasTwo counts && hasThree counts = generateChecksum xs (twos + 1) (threes + 1)
-    | hasTwo counts = generateChecksum xs (twos + 1) threes
-    | hasThree counts = generateChecksum xs twos (threes + 1)
-    | otherwise = generateChecksum xs twos threes
-    where counts = charCounts x
+findCommonLetters :: [String] -> String
+findCommonLetters [] = "U goofed"
+findCommonLetters (x:xs)
+    | [] == commonChars = findCommonLetters xs
+    | otherwise = commonChars
+    where commonChars = findDifferByOne x xs
+
